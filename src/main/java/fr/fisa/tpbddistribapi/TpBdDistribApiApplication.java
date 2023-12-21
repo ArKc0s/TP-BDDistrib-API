6a8e116fdf93d318a00ff169e2f71a9d7bd1bf19
Léo Wadin <wadinleo@gmail.com>
2023-12-21 14:43:27 +0100

    First Commit

---
+++ src/main/java/fr/fisa/tpbddistribapi/TpBdDistribApiApplication.java
@@ -0,0 +1,13 @@
+package fr.fisa.tpbddistribapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBdDistribApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpBdDistribApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/fisa/tpbddistribapi/TpBdDistribApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fisa.tpbddistribapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBdDistribApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
